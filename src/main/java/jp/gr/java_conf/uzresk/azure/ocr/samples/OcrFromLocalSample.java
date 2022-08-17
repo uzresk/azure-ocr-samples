38677b8029367d9e57b45847333899990faef8f1
uzresk <esaki.yuzuru@tis.co.jp>
2022-08-17 15:51:51 +0900

    change class name

---
+++ src/main/java/jp/gr/java_conf/uzresk/azure/ocr/samples/OcrFromLocalSample.java
@@ -0,0 +1,11 @@
+package jp.gr.java_conf.uzresk.azure.ocr.samples;
+
+public class OcrFromLocalSample {
+
+    public static void main(String[] args) {
+//        ComputerVisionUtils.readFromLocal("sample\\readsample.jpg");
+        ComputerVisionUtils.readFromLocal("sample\\PXL_20220817_025545304.jpg");
+//        ComputerVisionUtils.readFromLocal("sample\\PXL_20220817_044859892.jpg");
+    }
+
+}
